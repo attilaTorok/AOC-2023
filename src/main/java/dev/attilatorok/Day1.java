d22ef6440c130d4a8ff6fa03ba18622874b04854
Attila Török <ansansans8@gmail.com>
2023-12-04 17:26:07 +0100

    Solve Day1 first problem

--- src/main/java/dev/attilatorok/Day1.java
+++ src/main/java/dev/attilatorok/Day1.java
@@ -1,5 +1,8 @@
 package dev.attilatorok;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 //https://adventofcode.com/2023/day/1
 public class Day1 {
 
@@ -7,6 +10,29 @@ public class Day1 {
     }
 
     public int getAnswer(String fileName) {
+        int sum = 0;
+
+        try (BufferedReader reader = Utils.getBufferedReader(fileName)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sum += findNumber(line) * 10;
+                sum += findNumber(new StringBuilder(line).reverse().toString());
+            }
+
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        return sum;
+    }
+
+    public int findNumber(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isDigit(string.charAt(i))) {
+                return Character.getNumericValue(string.charAt(i));
+            }
+        }
+
         return 0;
     }
 
--- src/test/java/Day1Test.java
+++ src/test/java/Day1Test.java
@@ -7,9 +7,17 @@ public class Day1Test {
     private final Day1 app = new Day1();
 
     @Test
-    public void testDay1Example() {
+    public void testDay1_firstExample() {
         int answer = app.getAnswer("test/day1_first.txt");
         int expected = 142;
+
+        Assertions.assertEquals(expected, answer);
+    }
+
+    @Test
+    public void testDay1_firstInput() {
+        int answer = app.getAnswer("input/day1_first.txt");
+        int expected = 57346;
 
         Assertions.assertEquals(expected, answer);
     }
