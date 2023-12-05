45771c9e9b19ffc9a521bd35eae8d9359f9111bf
Attila Török <ansansans8@gmail.com>
2023-12-05 17:49:13 +0100

    Init Day3 part 2

---
+++ src/main/java/dev/attilatorok/Day3_2.java
@@ -0,0 +1,14 @@
+package dev.attilatorok;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Day3_2 extends Day3_1 {
+
+    public int getGearRatio(BufferedReader reader) throws IOException {
+        int ratio = 0;
+
+        return ratio;
+    }
+
+}
--- src/test/java/Day3Test.java
+++ src/test/java/Day3Test.java
@@ -1,4 +1,5 @@
 import dev.attilatorok.Day3_1;
+import dev.attilatorok.Day3_2;
 import dev.attilatorok.Utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import java.io.IOException;
 public class Day3Test {
 
     private final Day3_1 app1 = new Day3_1();
+    private final Day3_2 app2 = new Day3_2();
 
     @Test
     public void testPartSum() throws IOException {
@@ -35,6 +37,26 @@ public class Day3Test {
         try (BufferedReader reader = Utils.getBufferedReader("input/day3_part1.txt")) {
             int actual = app1.getPartSum(reader);
             int expected = 533784;
+
+            Assertions.assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void testGearRation() throws IOException {
+        try (BufferedReader reader = Utils.getBufferedReader("test/day3_first.txt")) {
+            int actual = app2.getGearRatio(reader);
+            int expected = 16345;
+
+            Assertions.assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void testGearRation1() throws IOException {
+        try (BufferedReader reader = Utils.getBufferedReader("test/day3_first_1.txt")) {
+            int actual = app2.getGearRatio(reader);
+            int expected = 467835;
 
             Assertions.assertEquals(expected, actual);
         }
