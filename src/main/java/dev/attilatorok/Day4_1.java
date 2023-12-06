563f48e782663b6699d879a2c1c15d61c8470cdf
Attila Török <ansansans8@gmail.com>
2023-12-06 16:58:51 +0100

    Solve Day4 part 1

--- src/main/java/dev/attilatorok/Day4_1.java
+++ src/main/java/dev/attilatorok/Day4_1.java
@@ -1,13 +1,53 @@
 package dev.attilatorok;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Day4_1 {
 
-    public int totalPoints() {
-        return 0;
+    public int totalPoints(BufferedReader reader) throws IOException {
+        int totalPoints = 0;
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            Card card = createCardFromLine(line);
+            totalPoints += pointsOnACard(card.winningNumbers, card.otherNumbers);
+        }
+
+        return totalPoints;
     }
 
-    public int pointsOnACard() {
-        return 0;
+    public int pointsOnACard(List<Integer> winningNumbers, List<Integer> otherNumbers) {
+        int sum = 0;
+
+        for (Integer winning : winningNumbers) {
+            if (otherNumbers.contains(winning)) {
+                if (sum == 0) {
+                    sum++;
+                } else {
+                    sum *= 2;
+                }
+            }
+        }
+
+        return sum;
     }
 
+    public Card createCardFromLine(String line) {
+        String[] lineSplit = line.split(":");
+        String[] firstPartSplit = lineSplit[0].split(" ");
+        int number = Integer.parseInt(firstPartSplit[firstPartSplit.length-1]);
+
+        String[] numbers = lineSplit[1].split("\\|");
+        List<Integer> winningNumbers = Arrays.stream(numbers[0].split(" ")).filter(s -> !"".equals(s)).map(Integer::valueOf).toList();
+        List<Integer> otherNumbers = Arrays.stream(numbers[1].split(" ")).filter(s -> !"".equals(s)).map(Integer::valueOf).toList();
+
+        return new Card(number, winningNumbers, otherNumbers);
+    }
+
+
+    public record Card (int number, List<Integer> winningNumbers, List<Integer> otherNumbers) {}
 }
--- src/test/java/Day4Test.java
+++ src/test/java/Day4Test.java
@@ -1,6 +1,11 @@
 import dev.attilatorok.Day4_1;
+import dev.attilatorok.Utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
 
 public class Day4Test {
 
@@ -8,18 +13,30 @@ public class Day4Test {
 
     @Test
     public void testPointsIn() {
-        int actual = app1.pointsOnACard();
+        int actual = app1.pointsOnACard(List.of(41, 48, 83, 86, 17), List.of(83, 86,  6, 31, 17,  9, 48, 53));
         int expected = 8;
 
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void testTotalPoints() {
-        int actual = app1.totalPoints();
-        int expected = 13;
+    public void testTotalPoints() throws IOException {
+        try (BufferedReader reader = Utils.getBufferedReader("test/day4_first.txt")) {
+            int actual = app1.totalPoints(reader);
+            int expected = 13;
 
-        Assertions.assertEquals(expected, actual);
+            Assertions.assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void inputTotalPoints() throws IOException {
+        try (BufferedReader reader = Utils.getBufferedReader("input/day4_part1.txt")) {
+            int actual = app1.totalPoints(reader);
+            int expected = 19855;
+
+            Assertions.assertEquals(expected, actual);
+        }
     }
 
 }
